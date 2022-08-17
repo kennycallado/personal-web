7bfcdd65dd8b8df486bb3cb17d8c6997a73bbd92
kennycallado <kennycallado@hotmail.com>
2022-08-17 17:14:56 +0200

    UPDATE Actualiza la página principal del sitio

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
+    /*
     colors: {
       'pink': '#ff00ff'
     },
+    */
   },
   plugins: [],
 }
