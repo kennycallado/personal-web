0bad00e97b30da582fa48baeae091ebb04d73895
kennycallado <kennycallado@hotmail.com>
2022-08-17 10:37:24 +0200

    FIX Cambia bootstrap por tailwind:   - quita bootstrap   - instala tailwind   - configura tailwind   - configura astro con tailwind   - modifica estilos en páginas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  server: { port: 8000 }
+  server: { port: 8000 },
+  integrations: [tailwind()]
 });
